83b9d2fb4d8e4b3a622b43f13cb8116b9b7eaf83
Sujit <sb2011ams@gmail.com>
2022-04-11 09:04:37 +0200

    added form sample and post handler

--- test-code/express-tut.js
+++ test-code/express-tut.js
@@ -3,13 +3,30 @@
 const express = require('express');
 app = express();
 
+// app.get('/', (req, res) => {
+//     res.send('Welcome to our homepage.')
+// })
+
 app.get('/', (req, res) => {
-    res.send('Welcome to our homepage.')
+    res.send(`
+    <h1>What color is the sky on a clear day?</h1>
+    <form action="/result" method="POST">
+        <input type="text name="color">
+        <button>Submit</>
+    </form>
+    `)
 })
 
 app.get('/about', (req, res) => {
     res.send('Welcome to our about page.')
 })
 
+app.get('/result', (req, res) => {
+    res.send('why are you trying to acccess this page.')
+})
+
+app.post('/result', (req, res) => {
+    res.send('Thanks for submitting the answer.')
+})
 
 app.listen(3000)
