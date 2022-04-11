76ffd4ff180591cc65f96a26f8abba867143875c
Sujit <sb2011ams@gmail.com>
2022-04-11 09:38:18 +0200

    2.4: added sample code for routing. books example.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
-
-app.listen(port);
-
+let topBooks = [
+    {
+      title: 'Harry Potter and the Sorcerer\'s Stone',
+      author: 'J.K. Rowling'
+    },
+    {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkien'
+    },
+    {
+      title: 'Twilight',
+      author: 'Stephanie Meyer'
+    }
+  ];
+  
+  // GET requests
+  app.get('/', (req, res) => {
+    res.send('Welcome to my book club!');
+  });
+  
+  app.get('/documentation', (req, res) => {                  
+    res.sendFile('public/documentation.html', { root: __dirname });
+  });
+  
+  app.get('/books', (req, res) => {
+    res.json(topBooks);
+  });
+  
+  
+  // listen for requests
+  app.listen(port, () => {
+    console.log(`Your app is listening on port ${port}.`);
+  })
