b369c8fbb7a1327c1a84f3b99ed4bd200643c3a4
Sujit <sb2011ams@gmail.com>
2022-04-11 11:30:32 +0200

    added morgan module; used it

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+
 const app = express();
+app.use(morgan('common'));
 
 const port = 3000;
 
